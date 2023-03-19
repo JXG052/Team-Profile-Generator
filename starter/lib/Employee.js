9c4a0ad70b648988e9538097a666e819eb1aaf64
JXG052 <jxg0521@gmail.com>
2023-03-19 14:52:39 +0000

    Installed necessary packages and completed Employee Object

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+       this.name = name,
+       this.id = id,
+       this.email = email 
+    }
+    getName(){
+        return this.name;
+    }
+    getId(){
+        return this.id;
+    }
+    getRole(){
+        return "Employee"
+    }
+
+}
+const jon = new Employee ("Jon", 1, "test@email.com")
+console.log(jon.id);
+
+
+
+module.exports = Employee // or export default es6 - same thing
